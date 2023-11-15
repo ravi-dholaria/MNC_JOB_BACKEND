13399b604ee741bc87bed3b09067b793fefe2269
ravi-dholaria <111003588+ravi-dholaria@users.noreply.github.com>
2023-11-15 13:07:48 +0530

    Update index.js
    configured port number

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
